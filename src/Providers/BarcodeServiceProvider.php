9fbddf45d646ca5bc489ba54cd1a18e1ad08205b
isahaq1 <hmisahaq01@gmail.com>
2025-07-07 16:22:35 +0600

    illuminate facad

--- src/Providers/BarcodeServiceProvider.php
+++ src/Providers/BarcodeServiceProvider.php
@@ -5,6 +5,9 @@ namespace Isahaq\Barcode\Providers;
 use Illuminate\Support\ServiceProvider;
 use Isahaq\Barcode\Services\BarcodeService;
 
+/**
+ * Laravel Service Provider for Isahaq\Barcode
+ */
 class BarcodeServiceProvider extends ServiceProvider
 {
     public function register()
@@ -12,5 +15,15 @@ class BarcodeServiceProvider extends ServiceProvider
         $this->app->singleton('barcode', function ($app) {
             return new BarcodeService();
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['barcode'];
     }
 } 
