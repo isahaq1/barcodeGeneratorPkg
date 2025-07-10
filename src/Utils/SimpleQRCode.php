7ec9abc656e2a4c25eefedb9f207e9b51d8de499
isahaq1 <hmisahaq01@gmail.com>
2025-07-10 17:44:27 +0600

    test

--- src/Utils/SimpleQRCode.php
+++ src/Utils/SimpleQRCode.php
@@ -9,16 +9,75 @@ namespace Isahaq\Barcode\Utils;
  */
 class SimpleQRCode
 {
-    public static function png(string $data, int $version = null, int $margin = 4, int $moduleSize = 4): string
+    private string $data;
+    private int $version;
+    private int $margin;
+    private int $moduleSize;
+    private $qr;
+
+    public function __construct(string $data, int $version = 2, int $margin = 4, int $moduleSize = 8)
     {
-        require_once __DIR__ . '/phpqrcode.php';
-        ob_start();
-        // If version is null, let the library auto-select
-        if ($version === null) {
-            \QRcode::png($data, null, QR_ECLEVEL_L, $moduleSize, $margin);
-        } else {
-            \QRcode::png($data, null, QR_ECLEVEL_L, $moduleSize, $margin, false, $version);
+        $this->data = $data;
+        $this->version = $version;
+        $this->margin = $margin;
+        $this->moduleSize = $moduleSize;
+    }
+
+    /**
+     * Generate the QR code matrix using a real encoder
+     */
+    public function generateMatrix(): array
+    {
+        require_once __DIR__ . '/qrcode-php-standalone.php'; // This file will contain the QR encoder class
+        $qr = new \QRCode();
+        $qr->setErrorCorrectLevel('L');
+        $qr->setTypeNumber($this->version);
+        $qr->addData($this->data);
+        $qr->make();
+        $size = $qr->getModuleCount();
+        $matrix = [];
+        for ($y = 0; $y < $size; $y++) {
+            for ($x = 0; $x < $size; $x++) {
+                $matrix[$y][$x] = $qr->isDark($x, $y) ? 1 : 0;
+            }
         }
-        return ob_get_clean();
+        $this->qr = $qr;
+        return $matrix;
+    }
+
+    public function renderPNG(): string
+    {
+        $matrix = $this->generateMatrix();
+        $size = count($matrix);
+        $imgSize = ($size + 2 * $this->margin) * $this->moduleSize;
+        $im = imagecreatetruecolor($imgSize, $imgSize);
+        $white = imagecolorallocate($im, 255, 255, 255);
+        $black = imagecolorallocate($im, 0, 0, 0);
+        imagefill($im, 0, 0, $white);
+        for ($y = 0; $y < $size; $y++) {
+            for ($x = 0; $x < $size; $x++) {
+                if ($matrix[$y][$x]) {
+                    imagefilledrectangle(
+                        $im,
+                        ($x + $this->margin) * $this->moduleSize,
+                        ($y + $this->margin) * $this->moduleSize,
+                        ($x + $this->margin + 1) * $this->moduleSize - 1,
+                        ($y + $this->margin + 1) * $this->moduleSize - 1,
+                        $black
+                    );
+                }
+            }
+        }
+        ob_start();
+        imagepng($im);
+        $pngData = ob_get_clean();
+        imagedestroy($im);
+        return $pngData;
+    }
+
+    public static function png(string $data, int $version = 2, int $margin = 4, int $moduleSize = 8): string
+    {
+        $qr = new self($data, $version, $margin, $moduleSize);
+        return $qr->renderPNG();
     }
 } 
