0a0d26bbd2b3dde80c753b3ffdc72ca5198555bc
isahaq1 <hmisahaq01@gmail.com>
2025-07-10 16:11:27 +0600

    qr not scan

---
+++ src/Utils/phpqrcode.php
@@ -0,0 +1,6 @@
+<?php
+// Minimal phpqrcode library for QR code generation (LGPL)
+// Source: https://sourceforge.net/projects/phpqrcode/
+// Only the QRcode::png() method is needed for integration.
+// ... (full qrlib.php code would be here, but for brevity, you should download the file from the official source)
+// Place the full contents of qrlib.php here. 
