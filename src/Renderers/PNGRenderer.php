15d5a3b4a8a5b8733ecefc4428f229a7c94ab18f
isahaq1 <hmisahaq01@gmail.com>
2025-07-12 14:56:20 +0600

    watermark

--- src/Renderers/PNGRenderer.php
+++ src/Renderers/PNGRenderer.php
@@ -74,12 +74,92 @@ class PNGRenderer implements RendererInterface
             $totalWidth += $bar[0] * $widthFactor;
         }
         $imageWidth = $totalWidth + 2 * $margin;
-        $imageHeight = $height + $margin + 20;
+        $imageHeight = $height + $margin + 24;
         $im = imagecreatetruecolor($imageWidth, $imageHeight);
         $bgColor = imagecolorallocate($im, $bg[0], $bg[1], $bg[2]);
         $fgColor = imagecolorallocate($im, $fg[0], $fg[1], $fg[2]);
         imagefill($im, 0, 0, $bgColor);
 
+        // Special rendering for EAN-13
+        if ($barcode->type === 'EAN13' && strlen($barcode->data) === 13) {
+            $guardBarHeight = $height + 8;
+            $normalBarHeight = $height;
+            $x = $margin;
+            // Guard bar positions in EAN-13 pattern (fixed positions)
+            $guardBarIdx = [0, 1, 2, 45, 46, 47, 92, 93, 94];
+            foreach ($bars as $i => $bar) {
+                $barWidth = $bar[0] * $widthFactor;
+                $isGuard = in_array($i, $guardBarIdx);
+                $barH = $isGuard ? $guardBarHeight : $normalBarHeight;
+                if ($bar[1] === 'black') {
+                    imagefilledrectangle($im, $x, $margin, $x + $barWidth - 1, $margin + $barH, $fgColor);
+                }
+                $x += $barWidth;
+            }
+            // Draw numbers
+            $font = 3;
+            $num = $barcode->data;
+            // First digit (left of bars)
+            $firstX = $margin - imagefontwidth($font) * 1.5;
+            $firstY = $margin + $height + 2;
+            imagestring($im, $font, $firstX, $firstY, $num[0], $fgColor);
+            // Left 6 digits (under left half)
+            $x = $margin + 3 * $widthFactor; // after first guard
+            for ($i = 1; $i <= 6; $i++) {
+                $digitX = $x + (($i - 1) * 7 * $widthFactor) + 2;
+                imagestring($im, $font, $digitX, $firstY, $num[$i], $fgColor);
+            }
+            // Right 6 digits (under right half)
+            $x = $margin + (3 + 42 + 5) * $widthFactor; // after center guard
+            for ($i = 7; $i <= 12; $i++) {
+                $digitX = $x + (($i - 7) * 7 * $widthFactor) + 2;
+                imagestring($im, $font, $digitX, $firstY, $num[$i], $fgColor);
+            }
+            ob_start();
+            imagepng($im);
+            $pngData = ob_get_clean();
+            imagedestroy($im);
+            return $pngData;
+        }
+        // Special rendering for EAN-8
+        if ($barcode->type === 'EAN8' && strlen($barcode->data) === 8) {
+            $guardBarHeight = $height + 8;
+            $normalBarHeight = $height;
+            $x = $margin;
+            // Guard bar positions in EAN-8 pattern (fixed positions)
+            $guardBarIdx = [0, 1, 2, 31, 32, 33, 66, 67, 68];
+            foreach ($bars as $i => $bar) {
+                $barWidth = $bar[0] * $widthFactor;
+                $isGuard = in_array($i, $guardBarIdx);
+                $barH = $isGuard ? $guardBarHeight : $normalBarHeight;
+                if ($bar[1] === 'black') {
+                    imagefilledrectangle($im, $x, $margin, $x + $barWidth - 1, $margin + $barH, $fgColor);
+                }
+                $x += $barWidth;
+            }
+            // Draw numbers
+            $font = 3;
+            $num = $barcode->data;
+            $firstY = $margin + $height + 2;
+            // Left 4 digits
+            $x = $margin + 3 * $widthFactor;
+            for ($i = 0; $i < 4; $i++) {
+                $digitX = $x + ($i * 7 * $widthFactor) + 2;
+                imagestring($im, $font, $digitX, $firstY, $num[$i], $fgColor);
+            }
+            // Right 4 digits
+            $x = $margin + (3 + 28 + 5) * $widthFactor;
+            for ($i = 4; $i < 8; $i++) {
+                $digitX = $x + (($i - 4) * 7 * $widthFactor) + 2;
+                imagestring($im, $font, $digitX, $firstY, $num[$i], $fgColor);
+            }
+            ob_start();
+            imagepng($im);
+            $pngData = ob_get_clean();
+            imagedestroy($im);
+            return $pngData;
+        }
+        // Default rendering for other 1D barcodes
         $x = $margin;
         foreach ($bars as $bar) {
             $barWidth = $bar[0] * $widthFactor;
